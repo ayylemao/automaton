30a55eec1d3e7a7a5d086b2843f11b02cb9d9abe
Matthias Vogler <matthias.vogler@coop.ch>
2023-08-09 12:46:28 +0200

    added bressenham

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -20,3 +20,39 @@ std::vector<int> utils::shuffleXOrder(int const grid_x)
     std::shuffle(xOrder.begin(), xOrder.end(), gen);
     return xOrder;
 }
+
+std::vector<std::tuple<int, int>> utils::bresenhamLine(int x1, int y1, int x2, int y2) 
+{
+	std::vector<std::tuple<int, int>> linePixels;
+
+	int dx = abs(x2 - x1);
+	int dy = abs(y2 - y1);
+	int sx = (x1 < x2) ? 1 : -1;
+	int sy = (y1 < y2) ? 1 : -1;
+	int err = dx - dy;
+
+	int maxPoints = dx + dy + 1;
+	linePixels.reserve(maxPoints);
+
+	while (true) {
+		linePixels.emplace_back(std::make_tuple(x1, y1));
+
+		if (x1 == x2 && y1 == y2) {
+			break;
+		}
+
+		int e2 = 2 * err;
+
+		if (e2 > -dy) {
+			err -= dy;
+			x1 += sx;
+		}
+
+		if (e2 < dx) {
+			err += dx;
+			y1 += sy;
+		}
+	}
+
+	return linePixels;
+}
